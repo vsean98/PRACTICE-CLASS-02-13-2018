c2c4cf2f45eb8b46534c765cb7a8d90a1bf49b0b
Cameron Hyman <cameronhyman@Camerons-MacBook-Air.local>
2018-02-13 14:20:01 -0500

    first commit

---
+++ Library.java
@@ -0,0 +1,20 @@
+package library;
+
+public class Library {
+String Library_Location;
+	int Library_Total_Books;
+	int _Staff_Total;
+	double _YearlyExpense;
+	public Library(String Library_Location2,int Library_Total_Books2,int _Staff_Total2,double _YearlyExpense2) {
+		// TODO Auto-generated constructor stub
+	
+		String Library_Location= Library_Location2;
+		int Library_Total_Books= Library_Total_Books2;
+		int _Staff_Total= _Staff_Total2;
+	
+		double _YearlyExpense=_YearlyExpense2;
+	
+	
+	}
+
+}
---
+++ Library_test.java
@@ -0,0 +1,235 @@
+package library;
+
+import java.util.Scanner;
+
+public class Library_test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println ("Please enter your library location ");
+		Scanner scanner=new Scanner(System.in);
+
+        String  anwser=scanner.nextLine();
+        	
+		
+    	
+		System.out.println ("Please enter your library total books ");
+		Scanner scanner1=new Scanner(System.in);
+
+        int anwser1=scanner1.nextInt();
+    	
+		
+        
+        System.out.println ("Please enter your library Staff Total  ");
+		Scanner scanner2=new Scanner(System.in);
+
+        int anwser2=scanner.nextInt();
+    	
+		
+        System.out.println ("Please enter your library Yearly Expenses ");
+		Scanner scanner3=new Scanner(System.in);
+
+        double  anwser3=scanner3.nextDouble();
+     
+        
+       
+
+		System.out.println ("Please enter your library location ");
+		Scanner scanner4=new Scanner(System.in);
+
+        String  anwser4=scanner4.nextLine();
+        	
+		
+    	
+		System.out.println ("Please enter your library total books ");
+		Scanner scanner5=new Scanner(System.in);
+
+        int anwser5=scanner5.nextInt();
+    	
+		
+        
+        System.out.println ("Please enter your library Staff Total  ");
+		Scanner scanner6=new Scanner(System.in);
+
+        int anwser6=scanner6.nextInt();
+    	
+		
+        System.out.println ("Please enter your library Yearly Expenses ");
+		Scanner scanner7=new Scanner(System.in);
+
+        double  anwser7=scanner7.nextDouble(); 
+        
+        
+        
+        
+        
+        
+
+		System.out.println ("Please enter your library location ");
+		Scanner scanner8=new Scanner(System.in);
+
+        String  anwser8=scanner8.nextLine();
+        	
+		
+    	
+		System.out.println ("Please enter your library total books ");
+		Scanner scanner9=new Scanner(System.in);
+
+        int anwser9=scanner9.nextInt();
+    	
+		
+        
+        System.out.println ("Please enter your library Staff Total  ");
+		Scanner scanner10=new Scanner(System.in);
+
+        int anwser10=scanner10.nextInt();
+    	
+		
+        System.out.println ("Please enter your library Yearly Expenses ");
+		Scanner scanner11=new Scanner(System.in);
+
+        double  anwser11=scanner11.nextDouble();
+        
+        
+        
+
+		System.out.println ("Please enter your library location ");
+		Scanner scanner12=new Scanner(System.in);
+
+        String  anwser12=scanner12.nextLine();
+        	
+		
+    	
+		System.out.println ("Please enter your library total books ");
+		Scanner scanner13=new Scanner(System.in);
+
+        int anwser13=scanner13.nextInt();
+    	
+		
+        
+        System.out.println ("Please enter your library Staff Total  ");
+		Scanner scanner14=new Scanner(System.in);
+
+        int anwser14=scanner14.nextInt();
+    	
+		
+        System.out.println ("Please enter your library Yearly Expenses ");
+		Scanner scanner15=new Scanner(System.in);
+
+        double  anwser15=scanner15.nextDouble();
+        
+        
+        
+        
+
+		System.out.println ("Please enter your library location ");
+		Scanner scanner16=new Scanner(System.in);
+
+        String  anwser16=scanner16.nextLine();
+        	
+		
+    	
+		System.out.println ("Please enter your library total books ");
+		Scanner scanner17=new Scanner(System.in);
+
+        int anwser17=scanner17.nextInt();
+    	
+		
+        
+        System.out.println ("Please enter your library Staff Total  ");
+		Scanner scanner18=new Scanner(System.in);
+
+        int anwser18=scanner18.nextInt();
+    	
+		
+        System.out.println ("Please enter your library Yearly Expenses ");
+		Scanner scanner19=new Scanner(System.in);
+
+        double  anwser19=scanner19.nextDouble();
+        
+        
+        
+        System.out.println(anwser);
+        
+        System.out.println(anwser1);
+        
+        System.out.println(anwser2);
+       
+        System.out.println(anwser3);
+        
+        System.out.println(anwser5);
+        
+        System.out.println(anwser6);
+        
+        System.out.println(anwser7);
+        
+        System.out.println(anwser8);
+        
+        System.out.println(anwser9);
+        
+        System.out.println(anwser10);
+        
+        System.out.println(anwser11);
+        
+        
+        System.out.println(anwser12);
+        
+        
+        System.out.println(anwser13);
+        
+        
+        System.out.println(anwser14);
+        
+        System.out.println(anwser15);
+        
+        System.out.println(anwser16);
+        
+        System.out.println(anwser17);
+        
+        System.out.println(anwser18);
+        
+        System.out.println(anwser19);
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+	}
+
+}
